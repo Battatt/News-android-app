c127b06a0363a05c67e1aafae6ef2a3dae01e715
battatt <battamaev@yandex.ru>
2026-09-10 22:22:02 +0300

    Domain layer update for settings screen

---
+++ app/src/main/java/com/example/news/domain/usecase/UpdateIsWifiOnlyUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.news.domain.usecase
+
+import com.example.news.domain.entity.Interval
+import com.example.news.domain.repository.SettingsRepository
+import javax.inject.Inject
+
+class UpdateIsWifiOnlyUseCase @Inject constructor(
+    private val settingsRepository: SettingsRepository
+) {
+    suspend operator fun invoke(isWifiOnly: Boolean) {
+        settingsRepository.updateIsWifiOnly(isWifiOnly)
+    }
+}
